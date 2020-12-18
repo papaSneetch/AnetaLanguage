1ad7718b850ef3cdc07c738d50f9aaf53c0059c0
papaSneetch <colin.bernhardt3@gmail.com>
2020-12-17 21:35:36 -0700

    Fixed global variables and while loops work.

--- test/bashScripts/annetaBuilderDebug.sh
+++ test/bashScripts/annetaBuilderDebug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 cd $(dirname $0)
-inputFile="../annetaBuilderTestData/test3"; 
+inputFile="../annetaBuilderTestData/test4"; 
 lldb -- ../../bin/annetaBuilder.bin $inputFile; 
--- test/bashScripts/annetaBuilderTest.sh
+++ test/bashScripts/annetaBuilderTest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 cd $(dirname $0)
 echo -n "AnnetaBuilder Matching Input Test: "; 
-inputFile="../annetaBuilderTestData/test3"; 
+inputFile="../annetaBuilderTestData/test4"; 
 ../../bin/annetaBuilder.bin $inputFile;
